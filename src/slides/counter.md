74022803f9fbef2a315edeb3966993f882e275b6
Stef Geraets <stef.geraets@eventix.nl>
2024-10-12 14:13:42 +0200

    Feat: Code highlighting for codeblocks and custom image component through mdsvex layout

---
+++ src/slides/counter.md
@@ -0,0 +1,18 @@
+---
+title: Counter Example
+description: First thing here to do post.
+date: '2023-4-14'
+categories:
+  - sveltekit
+  - svelte
+published: true
+---
+<script>
+  import Counter from '$lib/components/Counter.svelte'
+</script>
+
+## Counter
+
+The counter is rendered inside Markdown.
+
+<Counter />
--- svelte.config.js
+++ svelte.config.js
@@ -1,7 +1,27 @@
 import adapter from '@sveltejs/adapter-auto';
 import { vitePreprocess } from '@sveltejs/vite-plugin-svelte';
 
-import { mdsvex } from 'mdsvex'
+import { mdsvex, escapeSvelte } from 'mdsvex'
+import { createHighlighter } from 'shiki'
+
+/** @type {import('mdsvex').MdsvexOptions} */
+const mdsvexOptions = {
+	extensions: ['.md'],
+	layout: {
+		_: './src/mdsvex.svelte'
+	},
+	highlight: {
+		highlighter: async (code, lang = 'text') => {
+			const highlighter = await createHighlighter({
+				themes: ['github-dark'],
+				langs: ['javascript', 'typescript']
+			})
+			await highlighter.loadLanguage('javascript', 'typescript')
+			const html = escapeSvelte(highlighter.codeToHtml(code, { lang, theme: 'github-dark' }))
+			return `{@html \`${html}\` }`
+		}
+	},
+}
 
 /** @type {import('@sveltejs/kit').Config} */
 const config = {
@@ -10,9 +30,7 @@ const config = {
 	// for more information about preprocessors
 	preprocess: [
 		vitePreprocess(), 
-		mdsvex({ 
-			extensions: ['.md'],
-		})
+		mdsvex(mdsvexOptions)
 	],
 
 	kit: {
